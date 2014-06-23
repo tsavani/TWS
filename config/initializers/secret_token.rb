eb31bb024415b3ac272cc3013fca010b0142552d
Twinkal Savani <twinkal_savani@external.mckinsey.com>
2014-06-23 21:28:03 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TWS::Application.config.secret_key_base = '2b8c8c28209a416e21ecf8ae66517aa0f7535bce6dd97c32ee28f2701a76fa87d07a9a65c11b34c855247830db7c3186e15b098891e7d91677412c7292d87a55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TWS::Application.config.session_store :cookie_store, key: '_TWS_session'
